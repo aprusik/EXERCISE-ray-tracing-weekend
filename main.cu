aec5b780623972c9ecc806c04a331f674322b0b7
aprusik <29558544+aprusik@users.noreply.github.com>
2020-02-04 14:43:32 -0500

    add sphere and surface normals

--- main.cu
+++ main.cu
@@ -14,9 +14,32 @@ void check_cuda(cudaError_t result, char const *const func, const char *const fi
     }
 }
 
+__device__ float hit_sphere(const vec3& center, float radius, const ray& r) {
+    vec3 oc = r.origin() - center;
+    float a = dot(r.direction(), r.direction());
+    float b = 2.0f * dot(oc, r.direction());
+    float c = dot(oc, oc) - radius*radius;
+    float discriminant = b*b -4*a*c;
+    
+    if (discriminant < 0) { // no hit
+        return -1.0;
+    }
+    else {                  // hit
+        return (-b - sqrt(discriminant) ) / (2.0f*a);
+    }
+}
+
 __device__ vec3 color(const ray& r) {
+    // get location of (possible) sphere hit
+    float t = hit_sphere(vec3(0,0,-1), 0.5, r);
+    if (t > 0.0) {  // is a hit
+        // calculate surface normal at hit location
+        vec3 N = unit_vector(r.point_at_parameter(t) - vec3(0,0,-1));
+        return 0.5f*vec3(N.x()+1, N.y()+1, N.z()+1);
+    }
+    // no hit, so render background
     vec3 unit_direction = unit_vector(r.direction());
-    float t = 0.5f*(unit_direction.y() + 1.0);
+    t = 0.5f*(unit_direction.y() + 1.0);
     return (1.0f-t)*vec3(1.0, 1.0, 1.0) + t*vec3(0.5, 0.7, 1.0);
 }
 
