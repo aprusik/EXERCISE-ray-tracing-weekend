fd47acd27fce8c8b164cb0e5b1e7750de0801768
aprusik <29558544+aprusik@users.noreply.github.com>
2020-02-04 14:06:39 -0500

    1.4-cuda add rays, simple camera, lerp background

--- main.cu
+++ main.cu
@@ -1,4 +1,4 @@
-#include "vec3.h"
+#include "ray.h"
 
 #include <iostream>
 #include <fstream>
@@ -14,12 +14,22 @@ void check_cuda(cudaError_t result, char const *const func, const char *const fi
     }
 }
 
-__global__ void render(vec3 *fb, int max_x, int max_y) {
+__device__ vec3 color(const ray& r) {
+    vec3 unit_direction = unit_vector(r.direction());
+    float t = 0.5f*(unit_direction.y() + 1.0);
+    return (1.0f-t)*vec3(1.0, 1.0, 1.0) + t*vec3(0.5, 0.7, 1.0);
+}
+
+__global__ void render(vec3 *fb, int max_x, int max_y,
+                       vec3 lower_left_corner, vec3 horizontal, vec3 vertical, vec3 origin) {
     int i = threadIdx.x + blockIdx.x * blockDim.x;
     int j = threadIdx.y + blockIdx.y * blockDim.y;
     if ((i >= max_x) || j >= (max_y)) return;
     int pixel_index = j*max_x + i;
-    fb[pixel_index] = vec3(float(i) / max_x, float(j) / max_y, 0.2);
+    float u = float(i) / float(max_x);
+    float v = float(j) / float(max_y);
+    ray r(origin, lower_left_corner + u*horizontal + v*vertical);
+    fb[pixel_index] = color(r);
 }
 
 int main() {
@@ -28,6 +38,11 @@ int main() {
 
     int tx = 16;
     int ty = 16;
+
+    vec3 lower_left_corner(-2.0, -1.0, -1.0);
+    vec3 horizontal(4.0, 0.0, 0.0);
+    vec3 vertical(0.0, 2.0, 0.0);
+    vec3 origin(0.0, 0.0, 0.0);
 
     int num_pixels = nx*ny;
     size_t fb_size = num_pixels*sizeof(vec3);
@@ -39,7 +54,7 @@ int main() {
     // Render our buffer
     dim3 blocks(nx/tx+1, ny/ty+1);
     dim3 threads(tx,ty);
-    render<<<blocks, threads>>>(fb, nx, ny);
+    render<<<blocks, threads>>>(fb, nx, ny, lower_left_corner, horizontal, vertical, origin);
     checkCudaErrors(cudaGetLastError());
     checkCudaErrors(cudaDeviceSynchronize());
 
